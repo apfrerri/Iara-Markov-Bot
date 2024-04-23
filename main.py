c040b64b13a7efcc0f2c803febb20b3901dd4213
apfrerri <167873462+apfrerri@users.noreply.github.com>
2024-04-23 08:40:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import re, os, time, json, random
+from instagrapi import Client
+from random import randint
+import chatbot_module
+
+class Bot:
+    def __init__(self, username, password, data, data_markov):
+        self.cl = Client()
+        self.cl.login(username, password)
+        print(" [!] Login feito com sucesso!")
+
+        self.ai = chatbot_module.Chatbot()
+        self.ai.train_data(data, data_markov)
+
+    def pass_time(self, num=0):
+        if not num:
+            num = random.randint(30, 60)
+
+        time.sleep(random.randint(round(num / 2), num + round(num / 2)))
+
+    def direct_message(self, pattern="."):
+        for num in range(0, 10):
+            try:
+                thread = self.cl.direct_threads(selected_filter = "unread")[0]
+                print(" - Mensagem recebida: {}".format(thread.messages[0].text))
+
+                if not thread.messages[0].text:
+                    self.pass_time(150)
+                    break
+
+                if random.random() <= 0.03:
+                    self.cl.direct_send_photo(pattern + "/" + random.choice(os.listdir(pattern)), thread_ids=[thread.id])
+                    print(" [+] Imagem")
+                    self.pass_time(25)
+                else:
+                    msg = self.ai.get_answer_with_return(thread.messages[0].text)
+
+                    if msg:
+                        self.cl.direct_send(msg, thread_ids=[thread.id])
+                        print(" - Mensagem enviada! -> {} // {}".format(thread.messages[0].text, msg))
+                        self.pass_time(25)
+            except IndexError:
+                pass
+
+            self.pass_time(30)
+        self.cl.logout()
+
+iara = Bot("iaramarkov", "Instagram@I.a&v1d4!", json.loads(open("phrases.json", "rb").read().decode()), json.loads(open("database.json", "rb").read().decode()))
+iara.direct_message("Imagens")
---
+++ telegram.py
@@ -0,0 +1,22 @@
+import telebot
+import chatbot_module
+import json, random
+
+ai = chatbot_module.Chatbot()
+ai.train_data(json.loads(open("phrases.json", "rb").read().decode()), json.loads(open("database.json", "rb").read().decode()))
+
+bot = telebot.TeleBot("7172304282:AAFRKM4fJzNe4HihjQxBcnUBxXjIwmjxRQk")
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.reply_to(message, "Seja muito bem-vindo humano! Não sei o que te traz aqui, mas espero que a gente se divirta muito juntinhos! Tudo bem com você?")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    response_msg = ai.get_answer_with_return(message.text)
+    print(" - Mensagem: {} // {}".format(message.text, response_msg))
+
+    bot.reply_to(message, response_msg)
+
+
+bot.infinity_polling()
